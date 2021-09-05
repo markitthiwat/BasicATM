135b6e0086965f712e5f7b30618c1a664b902e92
golfdigg <mark.itthiwat@gmail.com>
2021-09-05 09:38:59 +0700

    1

---
+++ BasicATM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BasicATM
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int money;
+            do
+            {
+                Console.Write("Input Money = ");
+                int.TryParse(Console.ReadLine(), out money);
+            } while (money % 100 != 0); //verify money x or xx
+            Console.WriteLine("Money = {0}", money);
+            Console.WriteLine("Bank 1000 Baht= {0}", money / 1000);
+            Console.WriteLine("Bank 500 Baht= {0}", money / 500);
+            Console.WriteLine("Bank 100 Baht= {0}", money / 100);
+            Console.ReadKey();
+        }
+    }
+}
